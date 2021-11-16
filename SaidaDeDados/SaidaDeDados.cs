afd8d8d7c1439efd74e2af0337e551f72a3a32de
Ssousuke <wesleysousukee@gmail.com>
2021-11-16 11:03:40 -0300

    Adicionar arquivos de projeto.

---
+++ SaidaDeDados/SaidaDeDados.cs
@@ -0,0 +1,17 @@
+﻿class SaidaDeDados
+{
+    static void Main(string[] args)
+    {
+        int idade = 22;
+        double saldo = 1000.0;
+        string nome = "Wesley Matheus da Silva Farias";
+        // String format
+        Console.WriteLine("{0} tem {1} anos de idade e o saldo de {2} na conta", nome, idade, saldo);
+
+        // Interpolação
+        Console.WriteLine($"{nome} tem {idade} anos de idade e o saldo de {saldo} na conta");
+
+        // Concatenação
+        Console.WriteLine(nome + " tem " + idade + " anos de idade e o saldo de " + saldo + " na conta");
+    }
+}
